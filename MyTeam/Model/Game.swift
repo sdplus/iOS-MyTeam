69b2a84c75ea01089e532bdb276577424b7b41f5
sdplus <deplussofie@gmail.com>
2017-01-19 21:18:31 +0100

    First Commit

---
+++ MyTeam/Model/Game.swift
@@ -0,0 +1,19 @@
+//
+//  Game.swift
+//  MyTeam
+//
+//  Created by Sofie De Plus on 18/01/2017.
+//  Copyright © 2017 Sofie De Plus. All rights reserved.
+//
+
+import CoreLocation
+
+struct Game {
+    let homeTeam: String // eventueel aanpassen naar team
+    let awayTeam: String // eventueel aanpassen naar team
+    let location: CLLocationCoordinate2D
+    let address: String
+    let date: Date
+    let time: String
+    
+}
